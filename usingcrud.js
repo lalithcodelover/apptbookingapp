9135de11915d52b060a76b43e24ffd392061a446
lalithcodelover <lalith.vlr@gmail.com>
2022-10-22 16:45:58 +0530

    edit user added

--- usingcrud.js
+++ usingcrud.js
@@ -29,7 +29,7 @@ function showNewUserOnScreen(user) {
     const parentNode = document.getElementById('users');
     const childHTML=`<li id= ${user._id}> ${user.name} -${user.email}
                      <button onclick= deleteUser('${user._id}')>Delete User</button>
-                     <button onclick= editUserDetails('${user.email}','${user.name}','${user.mobile}')>Edit</button>
+                     <button onclick= editUserDetails('${user._id}','${user.email}','${user.name}','${user.mobile}')>Edit</button>
                      </li>`
     parentNode.innerHTML=parentNode.innerHTML + childHTML;                 
 }
@@ -67,10 +67,10 @@ function removeUserFromScreen(Id) {
 
 
 //Edit user
-function editUserDetails(email,name,mobile) {
+function editUserDetails(id,email,name,mobile) {
     document.getElementById('name').value = name;
     document.getElementById('email').value = email;
     document.getElementById('mobile').value = mobile;
     
-    deleteUser(email)
+   deleteUser(id)
 }
